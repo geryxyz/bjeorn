892147e1f8da9789778ee724a00a2978edf5c14d
Gergő Balogh <geryxyz@gmail.com>
2024-03-15 14:42:14 +0100

    feat: transcribing names into IPA and also determine the feature vectors of their segments

--- suggest_name.py
+++ suggest_name.py
@@ -1,27 +1,23 @@
 import re
 import subprocess
 from pathlib import Path
+from typing import NamedTuple
 
+import epitran
+import epitran.vector
 import pandas
 from tqdm import tqdm
 
 
-def phonetic_transcribe(name: str) -> tuple[str, ...]:
-    name = name.lower().replace('ő', 'ö').replace('ű', 'ü')  # espeak does not support hungarian special characters :(
-    raw_transcript = subprocess.check_output(f"espeak -q -X -v hu '{name}'", shell=True, encoding="utf-8")
-    transcript = []
-    for line in raw_transcript.split('\n'):
-        line = line.strip()
-        if "\t" in line:
-            columns = re.split(r'\s+', line)
-            transcript.append(columns[2].removeprefix('[').removesuffix(']'))
-    return tuple(transcript)
+class CharacterInfo(NamedTuple):
+    character_category: str
+    is_upper: int
+    orthographic_form: str
+    phonetic_form: str
+    segments: list[tuple]
 
 
 def load_names() -> pandas.DataFrame:
-    if Path('names.csv').is_file():
-        return pandas.read_csv('names.csv')
-
     male_names = pandas.read_csv('male.txt', header=None, names=['name'])
     male_names.loc[:, 'sex'] = 'male'
     female_names = pandas.read_csv('female.txt', header=None, names=['name'])
@@ -29,14 +25,24 @@ def load_names() -> pandas.DataFrame:
     all_names = pandas.concat((male_names, female_names), axis=0)
     all_names.reset_index(drop=True, inplace=True)
     tqdm.pandas(desc="Transcribing names", unit="names")
-    all_names.loc[:, 'transcript'] = all_names.loc[:, 'name'].progress_apply(phonetic_transcribe)
-    all_names.to_csv('names.csv', index=False)
+
+    epi = epitran.Epitran('hun-Latn')
+    all_names.loc[:, 'ipa'] = all_names.loc[:, 'name'].progress_apply(
+        lambda name: epi.transliterate(name)
+    )
+    all_names.loc[:, 'word_tuples'] = all_names.loc[:, 'name'].progress_apply(
+        lambda name: tuple(CharacterInfo(*raw) for raw in epi.word_to_tuples(name))
+    )
+    all_names.loc[:, 'segments'] = all_names.loc[:, 'word_tuples'].progress_apply(
+        lambda infos: tuple(segment for info in infos for segment in info.segments)
+    )
 
     return all_names
 
 
 def main():
     valid_names = load_names()
+    print()
 
 
 if __name__ == '__main__':
